882804f7c02170fc4867b1b05510f9571b1e3e30
Jessica <duellje@oregonstate.edu>
2021-03-01 14:51:53 -0500

    added notes and function with Vue

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const app = Vue.createApp({
       picture: "https://randomuser.me/api/portraits/men/10.jpg",
     };
   },
+  methods: {
+    getUser() {
+      this.firstName = "Sam";
+      this.lastName = "Smith";
+      this.email = "sam@gmail.com";
+      this.gender = "female";
+      this.picture = "https://randomuser.me/api/portraits/women/10.jpg";
+    },
+  },
 });
 
 app.mount("#app");
